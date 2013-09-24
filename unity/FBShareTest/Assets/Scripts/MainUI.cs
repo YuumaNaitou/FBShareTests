c9334435587aeab5da5ecd3808a548b02c4d981f
Ranier Montalbo <ranier.montalbo@klab.com>
2013-09-23 20:04:01 -0700

    Test login and post on Unity3D project

---
+++ unity/FBShareTest/Assets/Scripts/MainUI.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+using System.Collections;
+
+public class MainUI : MonoBehaviour {
+
+	private bool initialized = false;
+	private bool loggedIn = false;
+	private string scope = "publish_actions";
+	private Rect screenArea = new Rect();
+
+	// Use this for initialization
+	void Start () {
+		screenArea.width = Screen.width;
+		screenArea.height = Screen.height;
+		FB.Init (OnInitComplete, OnUnityWillHide);
+	}
+
+	void OnInitComplete() {
+		initialized = true;
+	}
+
+	void OnUnityWillHide(bool gameShown) {
+		Time.timeScale = gameShown ? 1f : 0f;
+	}
+
+	void OnGUI () {
+		GUILayout.BeginArea (screenArea);
+		GUILayout.BeginVertical ();
+		GUILayout.FlexibleSpace ();
+
+		if(initialized) {
+			GUILayout.BeginHorizontal ();
+			GUILayout.FlexibleSpace ();
+			string buttonLabel = FB.IsLoggedIn ? "Log Out" : "Log In";
+			if(GUILayout.Button(buttonLabel, GUILayout.Width(350), GUILayout.Height(100))) {
+				if(!FB.IsLoggedIn) {
+					FB.Login (scope, OnLoginComplete);
+				}else{
+					FB.Logout ();
+					loggedIn = false;
+				}
+			}
+			GUILayout.FlexibleSpace ();
+			GUILayout.EndHorizontal ();
+
+			/////////////////////////////
+			GUILayout.Space (100);
+			/////////////////////////////
+
+
+			GUILayout.BeginHorizontal ();
+			GUILayout.FlexibleSpace ();
+			if(loggedIn) {
+				if(GUILayout.Button("Share on Facebook", GUILayout.Width(350), GUILayout.Height(100))) {
+					OpenPostFeedDialog ();
+				}
+			}
+			GUILayout.FlexibleSpace ();
+			GUILayout.EndHorizontal ();
+		}else{
+			GUILayout.Label("Initializing...");
+		}
+
+		GUILayout.FlexibleSpace ();
+		GUILayout.EndVertical ();
+		GUILayout.EndArea ();
+	}
+
+	void OnLoginComplete(FBResult result) {
+		loggedIn = result.Error == null;
+	}
+
+	void OpenPostFeedDialog() {
+		string toId = "";
+		string link = "http://klab.com";
+		string linkName = "SpellChain";
+		string linkCaption = "Card Game";
+		string linkDescription = "Let's battle!";
+		string picture = "";
+		string mediaSource = "";
+		string actionName = "";
+		string actionLink = "";
+		string reference = "";
+		FB.Feed (toId, link, linkName, linkCaption, linkDescription, picture, mediaSource, actionName, actionLink, reference);
+	}
+}
